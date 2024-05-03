c2dd5641d9af1303ce4a38973808b6bcb7b99ff3
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-05-02 21:45:35 -0300

    hopefully no bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import parse_query from "./parse-query.js";
 
-import init, {World, Constraint, ConstraintShape, Vec2, PointCharge} from "./pkg/electron_simulator.js";
+import init, { World, Constraint, ConstraintShape, Vec2, PointCharge } from "./pkg/electron_simulator.js";
 await init();
 
 let width, height;
@@ -17,11 +17,12 @@ let width, height;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const iterations = Number(parse_query().iterations) || 1;
 const fps = Number(parse_query().fps) || Infinity;
 
 const constraint = new Constraint(
     ConstraintShape.Circle,
-    new Float64Array([ width / 2, height / 2, Math.min(width, height) / 2 ])
+    new Float64Array([width / 2, height / 2, Math.min(width, height) / 2])
 );
 const world = new World(9e9, constraint);
 
@@ -30,59 +31,27 @@ const world = new World(9e9, constraint);
  * @type {number[]}
  */
 let charges = [];
-charges.push(world.add_charge(new PointCharge(false, new Vec2(0.0, 0.0), new Vec2(0.0, 0.0))));
-console.log(World.get_charge(charges[0]));
+charges.push(world.add_charge(new PointCharge(false, new Vec2(0.0, 0.0), new Vec2(0.0, 0.0), 1)));
+charges.push(world.add_charge(new PointCharge(false, new Vec2(1.0, 0.0), new Vec2(1.0, 0.0), 1)));
 
-initialize_pendulum(amount);
 
-/** @type {Vec2 || null} */
-let last_pos = null;
-function render_pendulum() {
-    pendulum_ctx.clearRect(0, 0, width, height);
-
-    pendulum_ctx.lineWidth = 3;
-    pendulum_ctx.strokeStyle = "grey";
-    pendulum_ctx.beginPath();
-    for(let i = 0;i < points.length;i++) {
-        let point = World.get_point(points[i]);
-        if(i == 0) {
-            pendulum_ctx.moveTo(point.pos.x, point.pos.y);
-        } else {
-            pendulum_ctx.lineTo(point.pos.x, point.pos.y);
-        }
+function render_constraint() {
+    if (constraint.shape === ConstraintShape.Circle) {
+        ctx.fillStyle = "grey";
+        ctx.beginPath();
+        ctx.arc(constraint.get_data(0), constraint.get_data(1), constraint.get_data(2), 0, 2 * Math.PI);
+        ctx.fill();
     }
-    pendulum_ctx.stroke();
-
-    pendulum_ctx.fillStyle = "yellow";
-    for(let i = 0;i < points.length;i++) {
-        let point = World.get_point(points[i]);
-        pendulum_ctx.beginPath();
-        pendulum_ctx.arc(point.pos.x, point.pos.y, 6, 0, 2 * Math.PI);
-        pendulum_ctx.fill();
-
-        if(i == 0) {
-            pendulum_ctx.moveTo(point.pos.x, point.pos.y);
-        } else {
-            pendulum_ctx.lineTo(point.pos.x, point.pos.y);
-        }
-    }
-
-    let point = World.get_point(points[points.length - 1]);
-
-    if(!last_pos) last_pos = point.pos;
-
-    trail_ctx.strokeStyle = "blue";
-    trail_ctx.lineWidth = 1;
-
-    trail_ctx.beginPath();
-    trail_ctx.moveTo(last_pos.x, last_pos.y);
-    trail_ctx.lineTo(point.pos.x, point.pos.y);
-    trail_ctx.stroke();
-
-    last_pos = point.pos;
 }
-
-
+function render_charges() {
+    for (let ptr of charges) {
+        let charge = World.get_charge(ptr);
+        ctx.fillStyle = charge.charge > 0 ? "red" : "yellow";
+        ctx.beginPath();
+        ctx.arc(charge.pos.x, charge.pos.y, 10, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
 
 
 let start = Date.now();
@@ -95,21 +64,22 @@ document.onvisibilitychange = () => [visible, becameVisible] = [!visible, !visib
 
 function simulation_loop() {
     requestAnimationFrame(simulation_loop);
-    
-    if(!visible || becameVisible) {
+
+    if (!visible || becameVisible) {
         frame_count += fps * (Date.now() - last_frame) / 1000;
         last_frame = Date.now();
-        if(becameVisible) becameVisible = false;
-        if(fps == Infinity) return;
+        if (becameVisible) becameVisible = false;
+        if (fps == Infinity) return;
     }
 
-    if(1000 * frame_count / (Date.now() - start) > fps) return;
+    if (1000 * frame_count / (Date.now() - start) > fps) return;
     frame_count++;
 
     let dt = fps == Infinity ? (Date.now() - last_frame) / 1000 : 1 / fps;
     last_frame = Date.now();
 
-    render_pendulum();
+    render_constraint();
+    render_charges();
     world.step(dt, iterations);
 }
 requestAnimationFrame(simulation_loop);
