8bdab100a460adc5b65d9c2bea124dd40a047892
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2024-05-04 17:47:55 -0300

    trying to fix wasm compilation issue

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
                 test: /\.ts$/,
                 use: "ts-loader",
                 exclude: /node_modules/
+            },
+            {
+                test: /\.wasm$/,
+                type: "asset/inline"
             }
         ]
     },
